043b611f9969e3e5ead87063fb31525b8019eb41
Bruno Soares <vsoares.bruno@gmail.com>
2024-01-17 19:15:12 +0000

    refactor: archtecture

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="container pt-4"></div>;
+}
---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "PageForm (@CodeWithKliton)",
+  description: "Check my youtube channel for more content",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
